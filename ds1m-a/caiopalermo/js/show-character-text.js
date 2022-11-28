319bdae541a41f99c13e3f4764c159908bd54cc6
CLEITON DOS SANTOS CRUZ <22282081@CFP127ALUNO.LOCAL>
2022-11-28 11:24:02 -0300

    Personagens completo

--- ds1m-a/caiopalermo/js/show-character-text.js
+++
@@ -1,8 +0,0 @@
-function changeState(element) {
-  let display = document.getElementById(element).style.display;
-  if (display == "block") {
-    document.getElementById(element).style.display = "none";
-  } else {
-    document.getElementById(element).style.display = "block";
-  }
-}
